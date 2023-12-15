a30b355f400f0dcdea9bacace22c6fe8895c8b11
Adrián <abarmar0501@g.educaand.es>
2023-12-15 09:48:23 +0100

    Adición de contenido y mejoras del responsive y footer

--- js/homepage.js
+++ js/homepage.js
@@ -51,38 +51,13 @@ $(document).ready(function(){
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
         x.className += " responsive";
     } else {
-        x.className = "topnav";
+        x.className = "navbar";
     }
     }
 
-/************************ Modal ***********************/
-/* este código regula la ventana modal del footer. Debe estar aquí en la propia página porque no está encapsulado en una function y por tanto no se puede invocar en archivo aparte */
-// Get the modal
-var modal = document.getElementById("myModal");
+/************************************* Footer *******************************************/
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-if (event.target == modal) {
-    modal.style.display = "none";
-}
-}
